584e7d7518446fb53a1ec6ad0052ce3956ed3eac
batrakrishna20 <batrakrishna20@gmail.com>
2025-11-28 00:53:16 +0530

    Add array sum calculation program
    Implement a program to calculate the sum of array elements based on user input.

---
+++ Day29/Q57.c
@@ -0,0 +1,35 @@
+/*Q57: Find the sum of array elements.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+2 4 6 8
+Output 1:
+20
+
+Input 2:
+3
+1 1 1
+Output 2:
+3
+
+*/
+#include <stdio.h>
+int main(){
+
+ int arr[n];
+ int i , n;
+ int sum=0;
+
+ printf("Enter the input size of array");
+ scanf("%d", &n);
+ for(i=0;i<n;i++){
+    scanf("%d", &arr[i]);
+    sum += arr[i];
+ }
+ printf("%d\n",sum);
+
+
+    return 0;
+}
