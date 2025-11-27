5bd283865b19cf1db624e19e5828b8ae5b28860a
batrakrishna20 <batrakrishna20@gmail.com>
2025-11-28 00:34:01 +0530

    Add library fine calculation program with rules
    Implemented a program to calculate library fines based on the number of late days, with specific fine rates and cancellation criteria.

---
+++ Day12/Q23.c
@@ -0,0 +1,49 @@
+//Q23: Write a program to calculate a library fine based on late days.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+Fine ₹8
+
+Input 2:
+8
+Output 2:
+Fine ₹32
+
+Input 3:
+15
+Output 3:
+Fine ₹90
+
+Input 4:
+31
+Output 4:
+Membership Cancelled
+
+*/
+
+#include <stdio.h>
+int main(){
+int days;
+printf("Enter the number of days the book is late: ");
+scanf("%d",&days);
+
+if(days<=5){
+    printf("Fine ₹%d\n",days*2);
+}
+else if(days>5 && days<=10){
+    printf("Fine ₹%d\n",days*4);
+}
+else if(days>10 && days<=30){
+    printf("Fine ₹%d\n",days*6);
+}
+else{
+    printf("Membership Cancelled\n");
+
+
+}
+
+   return 0;
+}
